7e453db1f26fdadc397abf7d58d5946b34f00fff
EgorTitenkov <ominouzzzz@gmail.com>
2021-11-11 14:01:20 +0300

    First Task Comlete

---
+++ src/task1.ts
@@ -0,0 +1,21 @@
+let arr :Array<number>[] = [[1,2,3.5,4],[1,2.5,5.3],[0.5,0.6,2]]
+
+function sumAverage(arr:Array<Array<number>>){
+    const averageSum= averageValueArr(arr)
+    const result:number = Math.floor(averageSum)
+    return result
+}
+
+function averageValueArr(arr: Array<Array<number>>){
+    const averageValueArr = arr.map((innerArr) => {
+        return innerArr.reduce((acc,value) => acc + value) / innerArr.length
+    })
+    return averageSumValue(averageValueArr)
+}
+
+function averageSumValue(averageValueArr:Array<number>){
+    const averageSum = averageValueArr.reduce((acc,value) => acc + value)
+    return averageSum
+}
+
+console.log(sumAverage(arr))
---
+++ src/task2.ts
@@ -0,0 +1,4 @@
+
+function maxTripletSum(arr:Array<number>){
+
+}
