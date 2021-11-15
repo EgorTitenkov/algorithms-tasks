c52da22fd1a2aa76f785fe47b07b1b41093ed2d7
EgorTitenkov <ominouzzzz@gmail.com>
2021-11-15 12:01:50 +0300

    Add flattenAndSort function

---
+++ src/algorithms_and_functional_programming/flattenAndSort.ts
@@ -0,0 +1,8 @@
+const arr4:Array<Array<number>> = [[3,2,1],[4,6,5],[],[9,7,8]]
+
+function flattenAndSort(arr:Array<Array<number>>){
+    const unitedArr = [].concat(...arr).sort()
+    return unitedArr
+}
+
+console.log(flattenAndSort(arr4))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/string_functions/countDuplicates.ts',
+    entry: './src/algorithms_and_functional_programming/flattenAndSort.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
