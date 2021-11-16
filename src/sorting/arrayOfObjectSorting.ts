4d39d3911ad4cb7a63ea30d19da1513ccbcd880a
EgorTitenkov <ominouzzzz@gmail.com>
2021-11-16 17:35:13 +0300

    Add arrayOfObjectSorting function

---
+++ src/sorting/arrayOfObjectSorting.ts
@@ -0,0 +1,14 @@
+const obj = [
+    {"a": 1, "b": 3},
+    {"a": 3, "b": 2},
+    {"a": 2, "b": 40},
+    {"a": 4, "b": 12}
+]
+
+function arrayOfObjectSorting(list: Array<object>, sortBy: string) {
+    return list.sort(function (a, b) {
+        return b[sortBy] - a[sortBy]
+    })
+}
+
+console.log(arrayOfObjectSorting(obj, "a"))
