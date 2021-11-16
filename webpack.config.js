2f0646949c0d9eceda5d826c85ddafb322907710
EgorTitenkov <ominouzzzz@gmail.com>
2021-11-16 10:04:24 +0300

    Add compose function

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/algorithms_and_functional_programming/reduce.ts',
+    entry: './src/algorithms_and_functional_programming/compose.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
