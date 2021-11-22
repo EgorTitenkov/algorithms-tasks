1f6358d2ff845fd2186ae9993939fb91ae2487ba
EgorTitenkov <ominouzzzz@gmail.com>
2021-11-22 10:47:32 +0300

    Add mode to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,6 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, './dist'),
-        open: true,
-        compress: true,
-        hot: true,
-        port: 8080,
-    },
     entry: './src/index.ts',
     devtool: 'inline-source-map',
     module: {
