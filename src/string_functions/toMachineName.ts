b9773929d185447d8977d026e4c6f31a362ebd50
EgorTitenkov <ominouzzzz@gmail.com>
2021-11-29 14:30:56 +0300

    Fix toMachineName function

--- src/string_functions/toMachineName.ts
+++ src/string_functions/toMachineName.ts
@@ -1,17 +1,3 @@
-// function toMachineName(name: string, separator: string) {
-//    let nameArr = name.split(' ')
-//    const firstWord = nameArr[0]
-//       .split('')
-//       .filter((sym) => isNaN(+sym))
-//       .join('')
-//    nameArr[0] = firstWord
-//    return nameArr
-//       .join(' ')
-//       .split(' ')
-//       .filter((word) => word)
-//       .join(separator)
-//       .toLowerCase()
-// }
 
 export function toMachineName(name: string, separator: string) {
    let str = name
